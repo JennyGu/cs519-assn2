c5a98fead42f76f0bb51fc074b788b6be2a9c64a
Semidetached <semidetached@minime.org>
2012-10-16 15:42:18 -0400

    whoops... actually writes increasing values now

--- gen_testfile/gen_testfile.c
+++ gen_testfile/gen_testfile.c
@@ -2,17 +2,17 @@
 
 int main(void) {
 
-    unsigned int buffer[512 / sizeof(int)];
+    unsigned char buffer[512];
     unsigned int i, bufloc;
 
     FILE *fh = fopen("loop_block.tst", "w");
 
     for (i = 0; i < 256; i++) {
 
-        for (bufloc = 0; bufloc < (512 / sizeof(int)); bufloc++)
-            buffer[bufloc] = i & (i << 8) & (i << 16) & (i << 24);
+        for (bufloc = 0; bufloc < 512 ; bufloc++)
+            buffer[bufloc] = i;
 
-        fwrite((void *) buffer, 512, 1, fh);
+        fwrite((void *) &buffer, 512, 1, fh);
 
     }
     
