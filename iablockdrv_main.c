fe1b7833e405c0466d485060213e758ea3b318ad
Semidetached <semidetached@minime.org>
2012-10-17 14:19:20 -0400

    getting closer... added passthrough request function

--- iablockdrv_main.c
+++ iablockdrv_main.c
@@ -27,15 +27,29 @@ struct iablockdrv_dev {
 
 static struct iablockdrv_dev *drv_info;
 
+static int iablockdrv_make_request(struct request_queue *q, struct bio *bio) {
+   
+    /* Redirect the I/O request to our target device */
+    bio->bi_bdev = drv_info->target_dev;
+
+    /* A non-zero return value causes the request to be reissued */
+    return 1;
+}
+
 static int __init iablockdrv_init(void) {
     drv_info = kzalloc(sizeof(struct iablockdrv_dev), GFP_KERNEL);
+    drv_info-> target_dev = NULL;
     
     if (!drv_info) {
         return -ENOMEM;
     }
 
-	pr_info("Module loaded...\n");
-	pr_info("\tUsing %s as a target driver\n", target_device);
+	drv_info->major = register_blkdev(0, "iablockdrv");
+    if (drv_info->major <= 0) {
+        return -EBUSY;
+    }
+
+
 
 	if (NULL != target_device[0]) {
 	    drv_info->target_dev = blkdev_get_by_path(target_device, 
@@ -47,11 +61,19 @@ static int __init iablockdrv_init(void) {
         return -1;
     }
 
+	pr_info("Module loaded... major number: %u\n", drv_info->major);
+	pr_info("\tUsing %s as a target driver\n", target_device);
+
 	return 0;
 }
 
 static void __exit iablockdrv_exit(void)
 {
+    blkdev_put(drv_info->target_dev, FMODE_READ|FMODE_WRITE|FMODE_EXCL);
+
+    unregister_blkdev(drv_info->major, "iablockdrv");
+    kfree(drv_info);
+
 	pr_info("Module unloaded...\n");
 }
 
@@ -59,4 +81,3 @@ module_init(iablockdrv_init);
 module_exit(iablockdrv_exit);
 
 MODULE_LICENSE("GPL");
-
